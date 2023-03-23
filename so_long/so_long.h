c322ca9e6e3d162305c757737501bce08580fe70
Beta-J23 <76958823+Beta-J23@users.noreply.github.com>
2023-03-23 09:48:45 +0100

    Add files via upload

---
+++ so_long/so_long.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alpelliz <alpelliz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/17 11:44:26 by alpelliz          #+#    #+#             */
+/*   Updated: 2023/03/17 11:49:19 by alpelliz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+#define SO_LONG_H
+
+#include <unistd.h>
+#include <math.h>
+#include <stdlib.h>
+#include "ft_printf/ft_printf.h"
+#include "minilibx_opengl_20191021"
+
+#endif
+
