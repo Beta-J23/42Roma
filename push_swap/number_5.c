a77b33384cecdcba8c8a3fb932f57dfafdbfea0c
Beta-J23 <76958823+Beta-J23@users.noreply.github.com>
2023-04-18 12:49:36 +0200

    Add files via upload
    5 numbers ok

--- push_swap/number_5.c
+++ push_swap/number_5.c
@@ -6,7 +6,7 @@
 /*   By: alpelliz <alpelliz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 18:46:04 by alpelliz          #+#    #+#             */
-/*   Updated: 2023/04/17 21:29:35 by alpelliz         ###   ########.fr       */
+/*   Updated: 2023/04/18 12:22:55 by alpelliz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,7 @@ int		number_5(t_stack_a *a, t_stack_b *b)
 	pb(a, b);
 	array_printer(a, b);
 	i = 0;
+	//temp = 0;
 	while (i < a->size_of_int)
 	{
 		j = 0;
@@ -76,9 +77,13 @@ int		number_5(t_stack_a *a, t_stack_b *b)
 		i++;
 	}
 	ft_printf("Ï = %d\n", i);
-	pre_pb_5(a, b, i);
+	if (i > 2)
+		pre_pb_5(a, b, i + 1);
+	if (i <= 2)
+		pre_pb_5(a, b, i);
 	//pb(a, b);
 	pb(a, b);
+	ft_printf("\nTHIS IS ARRAY AFTER PB\n");
 	array_printer(a, b);
 	number_3(a, b);
 	pa(a, b);
@@ -97,21 +102,31 @@ int pre_pb_5(t_stack_a *a, t_stack_b *b, int x)
 	if (x == 1)
 	{
 		sa(a);
+		ft_printf("TEST X = 1\n");
 		return (1);
 	}
 	if (x == 2)
+	{
+		ft_printf("\nTHIS IS ARRAY BEFORE MOVES\n");
+		array_printer(a, b);
 		ra(a);
 		sa(a);
+		array_printer(a, b);
+		ft_printf("\nTHIS WAS ARRAY INTO AFTER MOVES\n");
+		ft_printf("TEST X = 2\n");
 		return (2);
+	}
 	if (x == 3)
 	{
 		rra(a);
 		rra(a);
+		ft_printf("TEST X = 3\n");
 		return (3);
 	}
 	if (x == 4)
 	{
 		rra(a);
+		ft_printf("TEST X = 4\n");
 		return (4);
 	}
 	return (0);
